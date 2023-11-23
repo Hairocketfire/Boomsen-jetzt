0130ddabef1388b74370553127862631d59c3d7a
Hairocketfire <jonas.berst@t-online.de>
2023-11-23 21:52:44 +0100

    add pocketbase 2

--- pb_public/js/main.js
+++ pb_public/js/main.js
@@ -1,8 +1,24 @@
 // Login Dialog
-document
-  .querySelectorAll('#loginDialog button')
-  .forEach(
-    (e) =>
-      (e.onclick = () =>
-        document.getElementById('loginDialog').classList.remove('active'))
-  )
+function submitLogin(e) {
+  e.preventDefault() // This prevents the form actually getting submitted
+  console.log(e)
+
+  const {username, password } = e.target.elements
+  console.log(username.value, password.value)
+
+  // If correct deactivate loginDialog
+  document.getElementById('loginDialog').classList.remove('active')
+
+  return false
+}
+
+// Register
+function submitRegister(e) {
+  e.preventDefault() // This prevents the form actually getting submitted
+  console.log(e)
+
+  // If correct deactivate loginDialog
+  document.getElementById('loginDialog').classList.remove('active')
+
+  return false
+}
