1f688e097abcbb3021a091fab8d08377d568ddd8
Hairocketfire <jonas.berst@t-online.de>
2023-11-25 23:57:48 +0100

    add map v1.0

---
+++ pb_public/js/map.js
@@ -0,0 +1,6 @@
+var map = L.map('map').setView([49.53698038461928, 8.013052737890826], 15);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
