64a9278e28d3aa772dd010ba7bcd3d2f818dce39
LoggeL <hyper.xjo@gmail.com>
2023-12-22 21:06:37 +0100

    Add simple navigation

--- pb_public/js/main.js
+++ pb_public/js/main.js
@@ -3,10 +3,9 @@ function submitLogin(e) {
   e.preventDefault() // This prevents the form actually getting submitted
   console.log(e)
 
-  const {username, password } = e.target.elements
+  const { username, password } = e.target.elements
   console.log(username.value, password.value)
 
-  
   // If correct deactivate loginDialog
   document.getElementById('loginDialog').classList.remove('active')
 
@@ -24,4 +23,19 @@ function submitRegister(e) {
   return false
 }
 
+// Navigation
+function navigateToPage(page) {
+  document.getElementById('content').innerHTML = ''
+  // Fetch page
+  fetch(`pages/${page}.html`)
+    .then((response) => response.text())
+    .then((html) => {
+      document.getElementById('content').innerHTML = html
+      // Load coresponing js
+      const script = document.createElement('script')
+      script.src = `pages/${page}.js`
+      document.body.appendChild(script)
+    })
+}
 
+navigateToPage('map')
