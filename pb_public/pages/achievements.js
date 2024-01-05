8aa323e682fcb90908965e4959a5b9fd80933a21
Hairocketfire <jonas.berst@t-online.de>
2024-01-06 00:38:41 +0100

    add confetti when achieved

--- pb_public/pages/achievements.js
+++ pb_public/pages/achievements.js
@@ -1,9 +1,13 @@
 function increaseProgress(progressId) {
-    const progressElement = document.getElementById(progressId);
-    const oldValue = progressElement.value 
-    progressElement.value = Math.min( oldValue + 10, 100) 
-    if (progressElement.value == 100){
-      progressElement.parentElement.classList.add("green")
-    }
+  const progressElement = document.getElementById(progressId);
+  if (progressElement.value == 100)
+    return
+  const oldValue = progressElement.value;
+  const step = parseInt(progressElement.getAttribute("step")) || 10;
+  progressElement.value = Math.min(oldValue + step, 100);
+  if (progressElement.value == 100) {
+    progressElement.parentElement.classList.add("green");
+    const jsConfetti = new JSConfetti();
+    jsConfetti.addConfetti();
+  }
 }
-
